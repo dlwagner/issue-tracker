95bfffb9d0027493bb2e312f3ab6c1bfd1e75ac3
dlwagner <wagner.don@gmail.com>
2019-12-30 02:44:13 -0500

    change desc to description

--- models/issue.js
+++ models/issue.js
@@ -1,16 +1,23 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
-
 
 var IssueSchema = new Schema(
     {
         title: { type: String, required: true, max: 100 },
-        desc: { type: String, required: true, max: 200 },
+        description: { type: String, required: true, max: 200 },
         created: { type: Date, default: Date.now },
         reporter: { type: Schema.Types.ObjectId, ref: 'User', required: true }
     }
 );
+
+// Virtual for created date
+IssueSchema
+    .virtual('created_formatted')
+    .get(function () {
+        return moment(this.created).format('MMM Do, YYYY');
+    });
 
 // Virtual for issue's URL
 IssueSchema
