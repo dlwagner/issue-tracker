3f4e3a28a55498af5d418760f414fcdd4c2507d1
dlwagner <wagner.don@gmail.com>
2020-02-03 11:21:57 -0500

    change callback for local dev and testing

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const strategy = new Auth0Strategy(
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-      process.env.AUTH0_CALLBACK_URL ||
+      // process.env.AUTH0_CALLBACK_URL ||
       'http://localhost:3000/callback',
   },
   (accessToken, refreshToken, extraParams, profile, done) =>
