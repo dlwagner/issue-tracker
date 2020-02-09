b6bd959505ec49924675d24370537f9831316e8c
dlwagner <wagner.don@gmail.com>
2020-02-09 10:45:08 -0500

    This file isnt being used any more, i dont know where or how these changes came about

--- routes/list.js
+++ routes/list.js
@@ -6,7 +6,6 @@ var router = express.Router();
 var issue_controller = require('../controllers/issueController');
 var user_controller = require('../controllers/userController');
 
-
 /// ISSUE ROUTES ///
 
 // GET issues home page.
@@ -14,19 +13,31 @@ var user_controller = require('../controllers/userController');
 router.get('/', issue_controller.index);
 
 // GET request for creating an issue. NOTE This must come before routes that display Issue (uses id).
-router.get('/issue/create', secured(), issue_controller.issue_create_get);
+router.get(
+  '/issue/create',
+  secured(),
+  issue_controller.issue_create_get,
+);
 
 // POST request for creating Issue.
 router.post('/issue/create', issue_controller.issue_create_post);
 
 // GET request to delete Issue.
-router.get('/issue/:id/delete', secured(), issue_controller.issue_delete_get);
+router.get(
+  '/issue/:id/delete',
+  secured(),
+  issue_controller.issue_delete_get,
+);
 
 // POST request to delete Issue.
 router.post('/issue/:id/delete', issue_controller.issue_delete_post);
 
 // GET request to update Issue.
-router.get('/issue/:id/update', secured(), issue_controller.issue_update_get);
+router.get(
+  '/issue/:id/update',
+  secured(),
+  issue_controller.issue_update_get,
+);
 
 // POST request to update Issue.
 router.post('/issue/:id/update', issue_controller.issue_update_post);
@@ -38,22 +49,33 @@ router.get('/issue/:id', secured(), issue_controller.issue_detail);
 router.get('/issues', secured(), issue_controller.issue_list);
 //router.get('/', secured(), issue_controller.issue_list);
 
-
 /// USERS ROUTES ///
 // GET request for creating User. NOTE This must come before route for id (i.e. display user).
-router.get('/user/create', secured(), user_controller.user_create_get);
+router.get(
+  '/user/create',
+  secured(),
+  user_controller.user_create_get,
+);
 
 // POST request for creating User.
 router.post('/user/create', user_controller.user_create_post);
 
 // GET request to delete User.
-router.get('/user/:id/delete', secured(), user_controller.user_delete_get);
+router.get(
+  '/user/:id/delete',
+  secured(),
+  user_controller.user_delete_get,
+);
 
 // POST request to delete User.
 router.post('/user/:id/delete', user_controller.user_delete_post);
 
 // GET request to update User.
-router.get('/user/:id/update', secured(), user_controller.user_update_get);
+router.get(
+  '/user/:id/update',
+  secured(),
+  user_controller.user_update_get,
+);
 
 // POST request to update User.
 router.post('/user/:id/update', user_controller.user_update_post);
