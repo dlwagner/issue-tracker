ff4c091ea68fdcf6d55d313e214bc9589317cc75
dlwagner <wagner.don@gmail.com>
2020-02-09 10:47:06 -0500

    modify to support new models

--- populatedb.js
+++ populatedb.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 
-console.log('This script populates some test issues to the database. Specified database as argument - e.g.: populatedb mongodb+srv://<username>:<password>@cluster0-mbdj7.mongodb.net/issue-tracker?retryWrites=true');
+console.log(
+  'This script populates some test issues to the database. Specified database as argument - e.g.: >sudo node populatedb mongodb+srv://<username>:<password>@cluster0-mbdj7.mongodb.net/issue-tracker?retryWrites=true',
+);
 
 // Get arguments passed on command line
 var userArgs = process.argv.slice(2);
@@ -10,342 +12,450 @@ if (!userArgs[0].startsWith('mongodb')) {
     return
 }
 */
-var async = require('async')
-var Issue = require('./models/issue')
-var User = require('./models/user')
-var Label = require('./models/label')
-var Environment = require('./models/environment')
-var Priority = require('./models/priority')
-var Severity = require('./models/severity')
-var Status = require('./models/status')
+var async = require('async');
+var Issue = require('./models/issue');
+var User = require('./models/user');
+var Project = require('./models/project');
+var Milestone = require('./models/milestone');
+var Priority = require('./models/priority');
+var Category = require('./models/category');
+var Status = require('./models/status');
 
 var mongoose = require('mongoose');
 var mongoDB = userArgs[0];
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on(
+  'error',
+  console.error.bind(console, 'MongoDB connection error:'),
+);
 
-var issues = []
-var users = []
-var labels = []
-var environments = []
-var priorities = []
-var severities = []
-var statuses = []
+var issues = [];
+var users = [];
+var statuses = [];
+var priorities = [];
+let projects = [];
+let categories = [];
+let milestones = [];
+// var projects = []
+// var milestones = []
+// var severities = []
 
-function issueCreate(title,
-    description,
-    label,
-    environment,
-    source_url,
-    created,
-    due_date,
-    reporter,
-    assignee,
-    priority,
-    severity,
-    steps_to_reproduce,
-    expected_results,
-    actual_results,
-    status,
-    cb) {
-    issuedetail = {
-        title: title,
-        description: description,
-        label: label,
-        environment: environment,
-        source_url: source_url,
-        created: created,
-        due_date: due_date,
-        reporter: reporter,
-        assignee: assignee,
-        priority: priority,
-        severity: severity,
-        steps_to_reproduce: steps_to_reproduce,
-        expected_results: expected_results,
-        actual_results: actual_results,
-        status: status
+function issueCreate(
+  title,
+  description,
+  author,
+  category,
+  priority,
+  assignee,
+  milestone,
+  project,
+  status,
+  cb,
+) {
+  issuedetail = {
+    title: title,
+    description: description,
+    author: author,
+    category: category,
+    priority: priority,
+    assignee: assignee,
+    milestone: milestone,
+    project: project,
+    status: status,
+  };
+
+  var issue = new Issue(issuedetail);
+
+  issue.save(function(err) {
+    if (err) {
+      cb(err, null);
+      return;
     }
-
-    var issue = new Issue(issuedetail);
-
-    issue.save(function (err) {
-        if (err) {
-            cb(err, null)
-            return
-        }
-        console.log('New Issue: ' + issue);
-        issues.push(issue)
-        cb(null, issue)
-    });
+    console.log('New Issue: ' + issue);
+    issues.push(issue);
+    cb(null, issue);
+  });
 }
 
 function statusCreate(status, cb) {
-    statusdetail = { status: status }
+  statusdetail = { status: status };
 
-    var status = new Status(statusdetail);
+  var status = new Status(statusdetail);
 
-    status.save(function (err) {
-        if (err) {
-            cb(err, null)
-            return
-        }
-        console.log('New Status: ' + status);
-        statuses.push(status)
-        cb(null, status)
-    });
+  status.save(function(err) {
+    if (err) {
+      cb(err, null);
+      return;
+    }
+    console.log('New Status: ' + status);
+    statuses.push(status);
+    cb(null, status);
+  });
 }
 
 function createStatuses(cb) {
-    async.series([
-        function (callback) {
-            statusCreate('new', callback);
-        },
-        function (callback) {
-            statusCreate('in-progress', callback);
-        },
-        function (callback) {
-            statusCreate('verified', callback);
-        },
-        function (callback) {
-            statusCreate('closed', callback);
-        },
-        function (callback) {
-            statusCreate('assigned', callback);
-        },
+  async.series(
+    [
+      function(callback) {
+        statusCreate('new', callback);
+      },
+      function(callback) {
+        statusCreate('in-progress', callback);
+      },
+      function(callback) {
+        statusCreate('verified', callback);
+      },
+      function(callback) {
+        statusCreate('closed', callback);
+      },
+      function(callback) {
+        statusCreate('assigned', callback);
+      },
     ],
-        // optional callback
-        cb);
+    // optional callback
+    cb,
+  );
 }
 
-function severityCreate(severity, cb) {
-    severitydetail = { severity: severity }
+function categoryCreate(category, cb) {
+  categorydetail = { category: category };
 
-    var severity = new Severity(severitydetail);
+  var category = new Category(categorydetail);
 
-    severity.save(function (err) {
-        if (err) {
-            cb(err, null)
-            return
-        }
-        console.log('New Severity: ' + severity);
-        severities.push(severity)
-        cb(null, severity)
-    });
+  category.save(function(err) {
+    if (err) {
+      cb(err, null);
+      return;
+    }
+    console.log('New Category: ' + category);
+    categories.push(category);
+    cb(null, category);
+  });
 }
 
-function createSeverities(cb) {
-    async.series([
-        function (callback) {
-            severityCreate('enhancement', callback);
-        },
-        function (callback) {
-            severityCreate('trivial', callback);
-        },
-        function (callback) {
-            severityCreate('minor', callback);
-        },
-        function (callback) {
-            severityCreate('major', callback);
-        },
-        function (callback) {
-            severityCreate('critical', callback);
-        },
+function createCategories(cb) {
+  async.series(
+    [
+      function(callback) {
+        categoryCreate('bug', callback);
+      },
+      function(callback) {
+        categoryCreate('feature', callback);
+      },
+      function(callback) {
+        categoryCreate('maintenance', callback);
+      },
     ],
-        // optional callback
-        cb);
+    // optional callback
+    cb,
+  );
 }
 
 function priorityCreate(priority, cb) {
-    prioritydetail = { priority: priority }
+  prioritydetail = { priority: priority };
 
-    var priority = new Priority(prioritydetail);
+  var priority = new Priority(prioritydetail);
 
-    priority.save(function (err) {
-        if (err) {
-            cb(err, null)
-            return
-        }
-        console.log('New Priority: ' + priority);
-        priorities.push(priority)
-        cb(null, priority)
-    });
+  priority.save(function(err) {
+    if (err) {
+      cb(err, null);
+      return;
+    }
+    console.log('New Priority: ' + priority);
+    priorities.push(priority);
+    cb(null, priority);
+  });
 }
 
 function createPriorities(cb) {
-    async.series([
-        function (callback) {
-            priorityCreate('low', callback);
-        },
-        function (callback) {
-            priorityCreate('medium', callback);
-        },
-        function (callback) {
-            priorityCreate('high', callback);
-        },
+  async.series(
+    [
+      function(callback) {
+        priorityCreate('low', callback);
+      },
+      function(callback) {
+        priorityCreate('medium', callback);
+      },
+      function(callback) {
+        priorityCreate('high', callback);
+      },
     ],
-        // optional callback
-        cb);
+    // optional callback
+    cb,
+  );
 }
 
-function environmentCreate(environment, cb) {
-    environmentdetail = { environment: environment }
+function milestoneCreate(milestone, cb) {
+  milestonedetail = { milestone: milestone };
 
-    var environment = new Environment(environmentdetail);
+  var milestone = new Milestone(milestonedetail);
 
-    environment.save(function (err) {
-        if (err) {
-            cb(err, null)
-            return
-        }
-        console.log('New Environment: ' + environment);
-        environments.push(environment)
-        cb(null, environment)
-    });
+  milestone.save(function(err) {
+    if (err) {
+      cb(err, null);
+      return;
+    }
+    console.log('New Milestone: ' + milestone);
+    milestones.push(milestone);
+    cb(null, milestone);
+  });
 }
 
-function createEnvironments(cb) {
-    async.series([
-        function (callback) {
-            environmentCreate('win10', callback);
-        },
-        function (callback) {
-            environmentCreate('ubuntu', callback);
-        },
-        function (callback) {
-            environmentCreate('greenNewDeal', callback);
-        },
-        function (callback) {
-            environmentCreate('borderWall', callback);
-        },
-        function (callback) {
-            environmentCreate('earth', callback);
-        },
+function createMilestones(cb) {
+  async.series(
+    [
+      function(callback) {
+        milestoneCreate('beta', callback);
+      },
+      function(callback) {
+        milestoneCreate('alpha', callback);
+      },
+      function(callback) {
+        milestoneCreate('development', callback);
+      },
+      function(callback) {
+        milestoneCreate('test', callback);
+      },
+      function(callback) {
+        milestoneCreate('production', callback);
+      },
     ],
-        // optional callback
-        cb);
+    // optional callback
+    cb,
+  );
 }
 
-function labelCreate(label, cb) {
-    labeldetail = { label: label }
+function projectCreate(project, cb) {
+  projectdetail = { project: project };
 
-    var label = new Label(labeldetail);
+  var project = new Project(projectdetail);
 
-    label.save(function (err) {
-        if (err) {
-            cb(err, null)
-            return
-        }
-        console.log('New Label: ' + label);
-        labels.push(label)
-        cb(null, label)
-    });
+  project.save(function(err) {
+    if (err) {
+      cb(err, null);
+      return;
+    }
+    console.log('New Project: ' + project);
+    projects.push(project);
+    cb(null, project);
+  });
 }
 
-function createLabels(cb) {
-    async.series([
-        function (callback) {
-            labelCreate('bug', callback);
-        },
-        function (callback) {
-            labelCreate('issue', callback);
-        },
-        function (callback) {
-            labelCreate('enhancement', callback);
-        },
-        function (callback) {
-            labelCreate('process', callback);
-        },
-        function (callback) {
-            labelCreate('ticket', callback);
-        },
+function createProjects(cb) {
+  async.series(
+    [
+      function(callback) {
+        projectCreate('alpha-one', callback);
+      },
+      function(callback) {
+        projectCreate('fizzbuster', callback);
+      },
+      function(callback) {
+        projectCreate('mars-lander', callback);
+      },
+      function(callback) {
+        projectCreate('apollo 11', callback);
+      },
+      function(callback) {
+        projectCreate('top-secret', callback);
+      },
     ],
-        // optional callback
-        cb);
+    // optional callback
+    cb,
+  );
 }
 
-function userCreate(nickName, displayName, aId, gravatar, provider, cb) {
-    userdetail = { nickName: nickName, displayName: displayName, aId: aId, gravatar: gravatar, provider: provider }
+function userCreate(
+  nickName,
+  displayName,
+  aId,
+  gravatar,
+  provider,
+  cb,
+) {
+  userdetail = {
+    nickName: nickName,
+    displayName: displayName,
+    aId: aId,
+    gravatar: gravatar,
+    provider: provider,
+  };
 
-    var user = new User(userdetail);
+  var user = new User(userdetail);
 
-    user.save(function (err) {
-        if (err) {
-            //cb(err, null)
-            //return
-        }
-        console.log('New User: ' + user);
-        users.push(user)
-        cb(null, user)
-    });
+  user.save(function(err) {
+    if (err) {
+      //cb(err, null)
+      //return
+    }
+    console.log('New User: ' + user);
+    users.push(user);
+    cb(null, user);
+  });
 }
 
 function createUsers(cb) {
-    async.series([
-        function (callback) {
-            userCreate('robsmith', 'robsmith@mail.com', 'auth0|5e0d3cc314eb2a0dd2967v8r', 'https://i2.wp.com/cdn.auth0.com/avatars/rs.png?ssl=1', 'auth0', callback);
-        },
-        function (callback) {
-            userCreate('markwilliams', 'markwilliams@hotmail.com', 'auth0|5e0d3cc314eb2a0dd2967k9l', 'https://i2.wp.com/cdn.auth0.com/avatars/mw.png?ssl=1', 'auth0', callback);
-        },
-        function (callback) {
-            userCreate('johndoe', 'johndoe@msmail.com', 'auth0|5e0d3cc314eb2a0dd2967y6m', 'https://i2.wp.com/cdn.auth0.com/avatars/jd.png?ssl=1', 'auth0', callback);
-        },
-        function (callback) {
-            userCreate('billwagner', 'bw@msmail.com', 'auth0|5e0d3cc314eb2a0dd2967d4p', 'https://i2.wp.com/cdn.auth0.com/avatars/bw.png?ssl=1', 'auth0', callback);
-        },
-        function (callback) {
-            userCreate('markwilliams', 'markwilliams@hotmail.com', 'auth0|5e0d3cc314eb2a0dd2967k9l', 'https://i2.wp.com/cdn.auth0.com/avatars/mw.png?ssl=1', 'auth0', callback);
-        },
+  async.series(
+    [
+      function(callback) {
+        userCreate(
+          'robsmith',
+          'robsmith@mail.com',
+          'auth0|5e0d3cc314eb2a0dd2967v8r',
+          'https://i2.wp.com/cdn.auth0.com/avatars/rs.png?ssl=1',
+          'auth0',
+          callback,
+        );
+      },
+      function(callback) {
+        userCreate(
+          'markwilliams',
+          'markwilliams@hotmail.com',
+          'auth0|5e0d3cc314eb2a0dd2967k9l',
+          'https://i2.wp.com/cdn.auth0.com/avatars/mw.png?ssl=1',
+          'auth0',
+          callback,
+        );
+      },
+      function(callback) {
+        userCreate(
+          'johndoe',
+          'johndoe@msmail.com',
+          'auth0|5e0d3cc314eb2a0dd2967y6m',
+          'https://i2.wp.com/cdn.auth0.com/avatars/jd.png?ssl=1',
+          'auth0',
+          callback,
+        );
+      },
+      function(callback) {
+        userCreate(
+          'billwagner',
+          'bw@msmail.com',
+          'auth0|5e0d3cc314eb2a0dd2967d4p',
+          'https://i2.wp.com/cdn.auth0.com/avatars/bw.png?ssl=1',
+          'auth0',
+          callback,
+        );
+      },
+      function(callback) {
+        userCreate(
+          'suzieQ',
+          'suzieQ@mail.com',
+          'auth0|5e0d3cc314eb2a0dd2967k9l',
+          'https://i2.wp.com/cdn.auth0.com/avatars/sq.png?ssl=1',
+          'auth0',
+          callback,
+        );
+      },
     ],
-        // optional callback
-        cb);
+    // optional callback
+    cb,
+  );
 }
 
 function createIssues(cb) {
-    async.series([
-        function (callback) {
-            issueCreate('Issue10', 'bug in machine', labels[1], environments[0], 'https://someurl/', '2019-11-8', '2020-02-15', users[0], users[1], priorities[2], severities[0], '1-2-3', 'success', 'fail', statuses[0], callback);
-        },
-        function (callback) {
-            issueCreate('Issue2', 'bug in coffee', labels[0], environments[1], 'https://someurl/', '2019-11-8', '2020-02-11', users[1], users[0], priorities[0], severities[2], '1-2-3', 'success', 'fail', statuses[1], callback);
-        },
-        function (callback) {
-            issueCreate('AC out', 'ac has stopped working', labels[2], environments[4], '', '2020-01-01', '2020-01-08', users[2], users[1], priorities[2], severities[4], '1-2-3', 'success', 'fail', statuses[3], callback);
-        },
-        function (callback) {
-            issueCreate('Process improvement', 'implement change mgt procedures', labels[3], environments[2], '', '2019-11-8', '2020-02-15', users[1], users[0], priorities[1], severities[0], '1-2-3', 'success', 'not implemented', statuses[2], callback);
-        },
-        function (callback) {
-            issueCreate('Issue1', 'racoon in garage', labels[1], environments[2], '', '2019-11-8', '2020-02-15', users[0], users[1], priorities[2], severities[3], '1-2-3', 'success', 'fail', statuses[0], callback);
-        },
-
+  async.series(
+    [
+      function(callback) {
+        issueCreate(
+          'Issue10',
+          'bug in machine',
+          users[0],
+          categories[0],
+          priorities[2],
+          users[1],
+          milestones[0],
+          projects[1],
+          statuses[0],
+          callback,
+        );
+      },
+      function(callback) {
+        issueCreate(
+          'Issue2',
+          'bug in coffee',
+          users[1],
+          categories[2],
+          priorities[0],
+          users[2],
+          milestones[3],
+          projects[2],
+          statuses[3],
+          callback,
+        );
+      },
+      function(callback) {
+        issueCreate(
+          'AC out',
+          'ac has stopped working',
+          users[4],
+          categories[0],
+          priorities[2],
+          users[2],
+          milestones[2],
+          projects[1],
+          statuses[0],
+          callback,
+        );
+      },
+      function(callback) {
+        issueCreate(
+          'process improvement',
+          'update change management procedures',
+          users[3],
+          categories[1],
+          priorities[1],
+          users[0],
+          milestones[1],
+          projects[3],
+          statuses[1],
+          callback,
+        );
+      },
+      function(callback) {
+        issueCreate(
+          'Issue1',
+          'racoon in garage',
+          users[2],
+          categories[0],
+          priorities[1],
+          users[0],
+          milestones[0],
+          projects[0],
+          statuses[0],
+          callback,
+        );
+      },
     ],
-        // optional callback
-        cb);
+    // optional callback
+    cb,
+  );
 }
 
-
-
-async.series([
+async.series(
+  [
     createStatuses,
-    createSeverities,
+    createCategories,
     createPriorities,
-    createEnvironments,
-    createLabels,
+    createMilestones,
+    createProjects,
     createUsers,
-    createIssues
-],
-    // Optional callback
-    function (err, results) {
-        if (err) {
-            console.log('FINAL ERR: ' + err);
-
-        }
-        else {
-            console.log('Issues: ' + issues);
-        }
-        // All done, disconnect from database
-        mongoose.connection.close();
-    });
+    createIssues,
+  ],
+  // Optional callback
+  function(err, results) {
+    if (err) {
+      console.log('FINAL ERR: ' + err);
+    } else {
+      console.log('Issues: ' + issues);
+    }
+    // All done, disconnect from database
+    mongoose.connection.close();
+  },
+);
