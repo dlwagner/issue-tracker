0304b708dd108c4ce50134af6d5244365eb0b557
dlwagner <wagner.don@gmail.com>
2020-02-24 10:44:40 -0500

    add file upload post and remove old unused code

--- routes/index.js
+++ routes/index.js
@@ -1,58 +1,23 @@
-var express = require('express');
-var secured = require('../lib/middleware/secured');
-var index_controller = require('../controllers/indexController');
-var router = express.Router();
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+/* eslint-disable prefer-template */
+const express = require('express');
+
+const db = require('../utils/db');
+
+const router = express.Router();
+const secured = require('../lib/middleware/secured');
+const indexController = require('../controllers/indexController');
 
 // Get home page
-// console.log("in index router");
-router.get('/', secured(), index_controller.index);
 
-// router.get('/newissue', secured(), index_controller.create_issue_get);
+router.get('/', secured(), indexController.index);
 
+// @route GET /newissue
 router.post(
   '/newissue',
-  secured(),
-  index_controller.create_issue_post,
+  db.upload.array('file', 3),
+  indexController.create_issue_post,
 );
 
-// GET request for creating new Issue. NOTE This must come before routes that display Issue (uses id).
-// router.get('/NewIssue', secured(), index_controller.new_issue);
-// POST request for creating new Issue.
-
-// GET request to delete Issue.
-
-// POST request to delete Issue.
-
-// GET request to update Issue.
-
-// POST request to update Issue.
-
-// GET request for one Issue.
-
 module.exports = router;
-
-/* GET home page. */
-/*
-router.get('/', function (req, res) {
-  res.redirect('/list');
-});
-*/
-
-/*
-router.get('/', secured(), function (req, res, next) {
-  res.redirect('/list');
-});
-*/
-/*
-router.get('/', function (req, res, next) {
-  //res.redirect('/list');
-  res.redirect('/');
-});
-*/
-
-/* GET home page. */
-/*
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Issue Tracker Home' });
-});
-*/
